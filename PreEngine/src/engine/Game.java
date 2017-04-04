cca444280222f2b7fb9009e92dabc20dfe93fdae
Luke Gehring <lukegehring99@gmail.com>
2017-04-04 13:31:34 -0500

    Added constructor to Game and Team class

--- PreEngine/src/engine/Game.java
+++ PreEngine/src/engine/Game.java
@@ -4,5 +4,35 @@ package engine;
 // This is known data
 
 public class Game {
+	
+	private Team team1;
+	private double team1Score;
+	
+	private Team team2;
+	private double team2Score;
+	
+	public Game(Team team1, double score1, Team team2, double score2)
+	{
+		this.team1 = team1;
+		this.team2 = team2;
+		team1Score = score1;
+		team2Score = score2;
+	}
+	
+	public double getDifference(Team comparingTeam){
+		if (team1 == comparingTeam)
+		{
+			return team1Score - team2Score;
+		}
+		else if(team2 == comparingTeam)
+		{
+			return team2Score - team1Score;
+		}
+		else
+		{
+			return (Double)null;
+		}
+		
+	}
 
 }
--- PreEngine/src/engine/Team.java
+++ PreEngine/src/engine/Team.java
@@ -1,5 +1,16 @@
 package engine;
 
 public class Team {
+	
+	private double averageDifference;
+	private int id;
+	
+	public Team(int id)
+	{
+		this.id = id;
+	}
 
+	
+	
+	
 }
