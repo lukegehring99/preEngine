472635bacf308a8fde650fe0cde8f289191828cb
Luke Gehring <lukegehring99@gmail.com>
2017-04-22 22:47:15 -0500

    Getters, setters, and Javadoc added

--- PreEngine/src/engine/Game.java
+++ PreEngine/src/engine/Game.java
@@ -1,16 +1,34 @@
 package engine;
 
-// This is a game between two different teams
-// This is known data
 
+/**
+ * Game Class 
+ * 
+ * This is a game between two different teams
+ * <p>
+ * (This is known data) 
+ * </p>
+ * 
+ * @author Luke Gehring
+ */
 public class Game {
 	
-	private Team team1;
-	private double team1Score;
+	private Team team1;              // Team 1 in the Game
+	private double team1Score;       // Points team 1 scored
 	
-	private Team team2;
-	private double team2Score;
+	private Team team2;              // Team 2 in the Game
+	private double team2Score;       // Points team 2 scored
 	
+	
+	/**
+	 * Game Constructor
+	 * Sets both teams and respective scores.
+	 * 
+	 * @param team1 The first team
+	 * @param score1 The first team's score
+	 * @param team2 The second team
+	 * @param score2 The second team's score
+	 */
 	public Game(Team team1, double score1, Team team2, double score2)
 	{
 		this.team1 = team1;
@@ -19,14 +37,22 @@ public class Game {
 		team2Score = score2;
 	}
 	
+	
+	/**
+	 * Gets the score difference between the two teams for the game. 
+	 * 
+	 * @param comparingTeam The team that is being compared to 
+	 * @return score difference between the teams (number needed to be added on to comparingTeam score to reach the other team score) 
+	 */
+	@SuppressWarnings("null")
 	public double getDifference(Team comparingTeam){
 		if (team1 == comparingTeam)
 		{
-			return team1Score - team2Score;
+			return team2Score - team1Score;
 		}
 		else if(team2 == comparingTeam)
 		{
-			return team2Score - team1Score;
+			return team1Score - team2Score;
 		}
 		else
 		{
@@ -35,10 +61,58 @@ public class Game {
 		
 	}
 	
-	public void addToGameField(GameField field)
+	
+	/**
+	 * Adds this Game to the GameField for the respective sport.
+	 * 
+	 * @param sport the game is to be added to
+	 */
+	public void addToGameField(Sport sport)
 	{
-		field.add(this);
+		sport.addToGameField(this);
 	}
+
+
+	/**
+	 * Getter for Team1Score
+	 * 
+	 * @return team1Score
+	 */
+	public double getTeam1Score() {
+		return team1Score;
+	}
+
+	
+	/**
+	 * Setter for Team1Score
+	 * 
+	 * @param team1Score replacement
+	 */
+	public void setTeam1Score(double team1Score) {
+		this.team1Score = team1Score;
+	}
+
+
+	/**
+	 * Getter for Team2Score
+	 * 
+	 * @return team2Score
+	 */
+	public double getTeam2Score() {
+		return team2Score;
+	}
+
+
+	/**
+	 * Setter for Team2Score
+	 * 
+	 * @param team2Score replacement
+	 */
+	public void setTeam2Score(double team2Score) {
+		this.team2Score = team2Score;
+	}
+	
+	
 	
 	
 
--- PreEngine/src/engine/Team.java
+++ PreEngine/src/engine/Team.java
@@ -2,21 +2,49 @@ package engine;
 
 public class Team {
 	
-	private double averageDifference;
-	private int id;
-	private String name;
+	private int id;        // Unique id representing the team
+	private String name;   // Name of the team
 	
+	
+	/**
+	 * Team Constructor
+	 * Sets the Team id and name.
+	 * 
+	 * @param id of the team
+	 * @param name of the team
+	 */
 	public Team(int id, String name)
 	{
 		this.id = id;
 		this.name = name;
 	}
 
+	
+	/**
+	 * Getter for the id of the team.
+	 * 
+	 * @return id of the team 
+	 */
 	public int getId()
 	{
 		return id;
 	}
 	
+	public String getName()
+	{
+		return name;
+	}
+	
+	
+	/**
+	 * Setter for the name of the team.
+	 * 
+	 * @param newName New name of the team
+	 */
+	public void setName(String newName)
+	{
+		name = newName;
+	}
 	
 	
 }
